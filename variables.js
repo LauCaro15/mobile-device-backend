987582530f1b156699ab6d39a5b6fc6ba563f012
LauCaro15 <lccandamil@gmail.com>
2023-08-31 19:44:37 -0500

    first commit

---
+++ variables.js
@@ -0,0 +1,21 @@
+PORT = 3000
+HOST = 'localhost'
+
+API_VERSION = 'v1'
+API_PATH = '/api/' + API_VERSION
+
+/* mongodb+srv://lauraccandamilc:<password>@cluster0.hghlgw1.mongodb.net/ */
+DB_HOST = 'cluster0.hghlgw1.mongodb.net/'
+USERNAME = 'lauraccandamilc'
+PASSWORD = 'lauraccandamilc'
+DB_NAME = 'myfirstprojectdb'
+
+module.exports ={
+    PORT,
+    HOST,
+    API_PATH,
+    DB_HOST,
+    USERNAME,
+    PASSWORD,
+    DB_NAME
+}
