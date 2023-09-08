3e01dae44eadef9d6f9a24b0777bd8e89d9f4b4c
LauCaro15 <lccandamil@gmail.com>
2023-09-07 19:45:55 -0500

    Implementation api queries

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const{HOST, PORT, API_PATH, DB_HOST, USERNAME,PASSWORD} = require("./variables");
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose")
+const app = require('./app')
+const {DB_HOST, USERNAME, PASSWORD , HOST, PORT, API_PATH} = require('./variables')
 
-/* app.listen(PORT, () => console.log(`Active port ${PORT}`));
+const connection_string = `mongodb+srv://${USERNAME}:${PASSWORD}@${DB_HOST}/`
 
-console.log(`http://${HOST}:${PORT}${API_PATH}`); */
-console.log(`mongodb+srv://${USERNAME}:${PASSWORD}@${DB_HOST}`);
-mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@${DB_HOST}` ,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() =>{
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => console.log(`Active port ${PORT}`));
-})
-.catch((err) => console.error(err));
-
-/* Habilitar middleware para request a db */
-app.use(bodyParser.json());
-app.use("/", (req, res) => {
-    /* http://localhost:3000/api/v1 */
-    res.send("Hola al dashboard");
-})
+mongoose
+    .connect(connection_string, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() =>{
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => console.log(`Active port ${PORT}`));
+    })
+    .catch((err)=>console.error(err))
